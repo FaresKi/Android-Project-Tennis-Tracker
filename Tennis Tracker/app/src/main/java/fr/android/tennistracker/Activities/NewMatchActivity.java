767fe383d4c71d20659c6b58058d49232d90f7fb
Fares Kissoum <kissoumfares@gmail.com>
2020-03-14 14:54:14 +0100

    Second Activity Layout
    Création du Layout de la deuxième activité (Nouveau Match).

--- Tennis Tracker/app/src/main/java/fr/android/tennistracker/Activities/NewMatchActivity.java
+++ Tennis Tracker/app/src/main/java/fr/android/tennistracker/Activities/NewMatchActivity.java
@@ -2,16 +2,13 @@ package fr.android.tennistracker.Activities;
 
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import androidx.appcompat.app.AppCompatActivity;
 import fr.android.tennistracker.R;
 
 public class NewMatchActivity extends AppCompatActivity {
-    private ListView listView;
-    private ArrayAdapter arrayAdapter;
 
-    private String[] players = {"Joueur 1", "Joueur 2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +16,11 @@ public class NewMatchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_match);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
 
-        listView = findViewById(R.id.joueurs);
-        arrayAdapter = new ArrayAdapter(this, R.layout.list_players,R.id.textView2,players);
-        listView.setAdapter(arrayAdapter);
-        
-        
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
