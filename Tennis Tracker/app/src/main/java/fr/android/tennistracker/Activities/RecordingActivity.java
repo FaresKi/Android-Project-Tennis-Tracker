b708ac0d3c398049a4fe2d299b2fb807892fd761
Fares Kissoum <kissoumfares@gmail.com>
2020-03-16 18:27:09 +0100

    last save & commit before folder relocation

--- Tennis Tracker/app/src/main/java/fr/android/tennistracker/Activities/RecordingActivity.java
+++ Tennis Tracker/app/src/main/java/fr/android/tennistracker/Activities/RecordingActivity.java
@@ -1,5 +1,7 @@
 package fr.android.tennistracker.Activities;
 
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,10 +14,17 @@ public class RecordingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recording);
         String firstName = getIntent().getStringExtra("firstPlayerName");
+        getSupportActionBar().setTitle(R.string.recordingActivityTitle);
         TextView tabFirstName = findViewById(R.id.tabFirstName);
         tabFirstName.setText(firstName);
         String secondName = getIntent().getStringExtra("secondPlayerName");
         TextView tabSecondName  = findViewById(R.id.tabSecondName);
         tabSecondName.setText(secondName);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_recording_activity, menu);
+        return true;
     }
 }
